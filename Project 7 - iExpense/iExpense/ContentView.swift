ad1d755f30ce62b4715dab34d5d882c532d952af
João Pedro da Mata Gonçalves Ribeiro <joaopedro.damata96@gmail.com>
2024-06-09 11:21:14 -0300

    Day 36: Project 7, part one

---
+++ Project 7 - iExpense/iExpense/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by João Pedro Mata on 06/06/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+	@Environment(\.dismiss) var dismiss
+	let name: String
+
+	var body: some View {
+		Text("Hello, \(name)!")
+		Button("dismiss") {
+			dismiss()
+		}
+	}
+}
+
+struct ContentView: View {
+	@State private var showingSheet = false
+
+    var body: some View {
+		Button("Show sheet") {
+			showingSheet.toggle()
+		}
+		.sheet(isPresented: $showingSheet) {
+			SecondView(name: "John")
+		}
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 7 - iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by João Pedro Mata on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
